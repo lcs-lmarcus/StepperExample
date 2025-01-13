b662e9d735a7c16aa43c0543b9c5bcb01448495d
Marcus Li <mmli@lcs.on.ca>
2025-01-13 08:47:05 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Marcus Li on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
